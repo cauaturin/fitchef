0b856352ae7d3dcedadc3cca471596976b1894b7
victor <victoriuskriegen@gmail.com>
2019-12-10 16:58:00 -0300

    versão final aula 10/12

--- www/view/FRONT-header.php
+++ www/view/FRONT-header.php
@@ -8,6 +8,8 @@
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="<?php echo $url; ?>/fontawesome/css/all.css">
   <link rel="stylesheet" href="<?php echo $url; ?>/css/style.css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 
 <body>
--- www/view/FRONT-logincadastro.php
+++ www/view/FRONT-logincadastro.php
@@ -1,72 +1,35 @@
 <?php require_once "FRONT-header.php"; ?>
 
-<div class="sectiongray">
-<div class="container login">
-
-	<div class="row">
 	
+		<div class="login-container login-form-1">
+		
+                    <h3>Login</h3>
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Seu Email *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Sua senha *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btnSubmit" value="Login" />
+                        </div>
+                        <div class="form-group">
+                            <a href="#" class="ForgetPwd">Esqueceu sua senha?</a>
+                        </div>
+                    </form>
+                </div>
+				
 
-		<div class="col-sm-6 login-detalhes">
-			<form method="post" action="<?php echo $url;?>/cliente/logar" class="form-horizontal">
-			<fieldset>
-
-			<!-- Form Name -->
-			<legend>Login</legend>
-
-			<!-- Text input-->
-			<div class="form-group">
-			  <label class="col-md-4 control-label" for="email">E-mail</label>  
-			  <div class="col-md-9">
-			  <input id="email" name="email" type="text" placeholder="" class="form-control input-md" required="">
-			    
-			  </div>
-			</div>
-
-			<!-- Password input-->
-			<div class="form-group">
-			  <label class="col-md-4 control-label" for="senha-login">Senha</label>
-			  <div class="col-md-9">
-			    <input id="senha" name="senha" type="password" placeholder="" class="form-control input-md" required="">
-			    <a href="#"><span class="forget help-block">Esqueci minha senha</span></a>
-			  </div>
-			</div>
-
-			<div class="form-group">
-			  <label class="col-md-4 control-label" for="btnregister"></label>
-			  <div class="col-md-9">
-			    <button id="btnregister" name="btnregister" class="btn">Entrar</button>
-			  </div>
-			
-			</div>
-			</fieldset>
-			</form>
-
-		</div>
+</div>
+</div>
 
 
-	<div class="col-sm-6 login-detalhes">
 
-			<form method="post" action="<?php echo $url;?>/cliente/cadastrar" class="form-horizontal">
-			<fieldset>
-
-			<!-- Form Name -->
-			<legend>Cadastre-se</legend>
 
 			
 <?php include "mensagem.php"; ?>
 
 		
-			</fieldset>
 
-
-
-			</form>
-
-
-			</div>
-			</div>
-
-</div>
-</div>
-</div>
 <?php include "FRONT-footer.php" ?>
