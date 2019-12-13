946af731ffcf666b0c50e03f9aa7e067babe1b84
victor <victoriuskriegen@gmail.com>
2019-12-13 14:04:57 -0300

    começo aula 13/12

---
+++ www/API/PedidoCadastrar.php
@@ -0,0 +1,30 @@
+<?php
+namespace FITCHEF\API;
+use FITCHEF\Model\Cliente;
+use FITCHEF\Model\Pedido;
+use FITCHEF\Model\Carrinho;
+use FITCHEF\DAO\DAOPedido;
+class PedidoCadastrar{
+    public $msg;
+    function __construct(){
+           
+     try{
+        $c = new Cliente();
+        $c->setId(4); // INSERIR UM ID JÁ EXISTENTE
+        $c->setNome('Daniel');
+    
+         $obj = new Pedido();
+         //$c->setId();
+         $obj->setData('2019-12-25');
+         $obj->setFrete(20.00);
+         $obj->setDias(5);
+         $obj->setCliente($c);
+        
+        $DAO = new DAOPedido();
+        $this->msg = $DAO->cadastrar($obj,$_SESSION['carrinho']);
+        echo $this->msg;
+    }catch(Exception $e){
+        $this->msg = $e->getMessage();
+}
+    }
+}
